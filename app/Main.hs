ee06da63fd5295d2fb06747e313d1693a0332cf2
Jim Strieter <jimstrieter@gmail.com>
2020-07-25 12:11:14 -0400

    Made paths self explanatory

--- app/Main.hs
+++ app/Main.hs
@@ -2,13 +2,10 @@ module Main where
 import Rendering.GenerateTestbench
 import Rendering.ProcessFile
 
---main :: IO ()
---main = putStrLn "yo" 
 
-inputFile = "/home/jamie/hask-to-vhdl/src/TestVhd/test.vhd"
-outputFile = "/home/jamie/hask-to-vhdl/src/TestVhd/test_tb.vhd"
+inputFile = "/path/to/some/vhd.vhd"
+outputFile = "/path/to/some/other/vhd.vhd"
 
---main :: IO()
 main = do
     putStrLn "Running main..."
     let los = generateTestbench
