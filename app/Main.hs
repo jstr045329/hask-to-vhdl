c9c2792b13e8c361bd6b052943c1d7e20634e1a6
Jim Strieter <jimstrieter@gmail.com>
2020-07-25 12:33:01 -0400

    Commented out names in  Main.hs to avoid stepping on descriptive names the user might want to use.

--- app/Main.hs
+++ app/Main.hs
@@ -3,12 +3,12 @@ import Rendering.GenerateTestbench
 import Rendering.ProcessFile
 
 
-inputFile = "/path/to/some/vhd.vhd"
-outputFile = "/path/to/some/other/vhd.vhd"
+--inputFile = "/path/to/some/vhd.vhd"
+--outputFile = "/path/to/some/other/vhd.vhd"
 
 main = do
     putStrLn "Running main..."
-    let los = generateTestbench
-    processFile inputFile outputFile generateTestbench
+--    let los = generateTestbench
+--    processFile inputFile outputFile generateTestbench
 
 
