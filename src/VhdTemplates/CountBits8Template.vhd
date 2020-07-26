68d214ee5e6efe2e9007b4c7408f7bf1b1e7af2b
Jim Strieter <jimstrieter@gmail.com>
2020-07-26 14:54:44 -0400

    Made MakeBitCountingLogic.py correctly populate a template, like all the other scripts

---
+++ src/VhdTemplates/CountBits8Template.vhd
@@ -0,0 +1,61 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+
+entity count1s is
+    generic (
+        use_async_reset : std_logic := '1'
+    );
+    port (
+        clk : in std_logic;
+        reset : in std_logic;
+        a0 : in std_logic := '0';
+        a1 : in std_logic := '0';
+        a2 : in std_logic := '0';
+        a3 : in std_logic := '0';
+        a4 : in std_logic := '0';
+        a5 : in std_logic := '0';
+        a6 : in std_logic := '0';
+        a7 : in std_logic := '0';
+        q : out std_logic_vector(3 downto 0)
+    );
+end count1s;
+
+
+architecture behavioral_count1s of count1s is
+
+signal agg : std_logic_vector(3 downto 0);
+
+<insert_logic_here>
+
+begin
+
+agg <= a0 & a1 & a2 & a3 & a4 & a5 & a6 & a7;
+
+USE_ASYNC_RESET_BLOCK: if use_async_reset = '1' generate 
+    process(clk, reset)
+    begin
+        if reset = '1' then
+            q <= (others => '0');
+        elsif rising_edge(clk) then
+            q <= count_bits(agg);
+        end if;
+    end process;
+end generate;
+
+USE_SYNC_RESET_BLOCK: if use_async_reset = '0' generate
+    process(clk)
+    begin
+        if rising_edge(clk) then
+            if reset = '1' then
+                q <= (others => '0');
+            else
+                q <= count_bits(agg);
+            end if;
+        end if;
+    end process;
+end generate;
+
+end behavioral_count1s;
+
+
--- src/py/MakeBitCountingLogic.py
+++ src/py/MakeBitCountingLogic.py
@@ -1,6 +1,7 @@
-# This script does not write to any files. 
-# It prints to console. 
-# Copy and paste. 
+# This script generates a function that counts the number of 1's in 
+# a std_logic_vector.
+INPUT_FILE = "../VhdTemplates/CountBits8Template.vhd"
+OUTPUT_FILE = "../VhdLibs/CountBits8.vhd"
 NUM_INPUT_BITS = 8
 NUM_OUTPUT_BITS = 4
 
@@ -43,14 +44,15 @@ def main():
     y.append("variable y : std_logic_vector(%d downto 0);" % (NUM_OUTPUT_BITS-1,))
     y.append("begin")
     y.append(tab() + "y := (others => '0');")
+    y.append(tab() + "case x is")
     for i in range(2**NUM_INPUT_BITS):
         oneBinStr = int2Bin(i)
         s1 = tab() + "when " + wrapNum(zeroPad(oneBinStr, NUM_INPUT_BITS)) + " =>"
-        s2 = tab(2) + "y <= " + wrapNum(zeroPad(int2Bin(count1s(oneBinStr)), 4)) + ";"
+        s2 = tab(2) + "y := " + wrapNum(zeroPad(int2Bin(count1s(oneBinStr)), 4)) + ";"
         y.append(s1)
         y.append(s2)
     y.append(tab() + "when others =>")
-    y.append(tab(2) + "y <= (others => '0');")
+    y.append(tab(2) + "y := (others => '0');")
     y.append(tab() + "end case;")
     y.append(tab() + "return y;")
     y.append("end function;")
@@ -59,9 +61,24 @@ def main():
 
 
 if __name__ == "__main__":
-    los = main()
-    for line in los:
+    logicLines = main()
+    templateLines = []
+    for line in logicLines:
         print(line)
+    with open(INPUT_FILE, 'r') as f:
+        templateLines.extend(f.readlines())
+    y = []
+    for line in templateLines:
+        los = line.split()
+        if len(los) > 0 and los[0] == "<insert_logic_here>":
+            y.extend([s + "\n" for s in logicLines])
+        else:
+            y.append(line)
+
+    with open(OUTPUT_FILE, 'w') as f:
+        for line in y:
+            print(line)
+            f.write(line)
 
 
 
