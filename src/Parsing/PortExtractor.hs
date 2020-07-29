0c2d1e75a4cb4af2def41cfa38bcd79dd2ff6bb4
Jim Strieter <jimstrieter@gmail.com>
2020-07-29 11:39:29 -0400

    Possible fix for default extraction issue

--- src/Parsing/PortExtractor.hs
+++ src/Parsing/PortExtractor.hs
@@ -111,16 +111,16 @@ genericHasDefault :: [String] -> Bool
 genericHasDefault xs
     -- TODO: Try changing 4 to 6 and see if that breaks anything.
     | length xs <= 4                = False
-    -- | firstDefaultAssignment xs 0 == (-1) = False
+    | firstDefaultAssignment xs 0 == (-1) = False
     | firstSemicolon xs 0 == (-1)   = (firstDefaultAssignment xs 0) > 0
     | otherwise                     = (firstSemicolon xs 0) > (firstDefaultAssignment xs 0) 
                             
 
 portHasDefault :: [String] -> Bool
 portHasDefault xs 
-    | (length xs <= 7)                = False
+    | (length xs <= 7)              = False
     | (firstDefaultAssignment xs 0 == (-1)) = False
-    | (firstSemicolon xs 0 == (-1))   = (firstDefaultAssignment xs 0) > 0
+    | (firstSemicolon xs 0 == (-1)) = (firstDefaultAssignment xs 0) > 0
     | otherwise                     = (firstSemicolon xs 0) > (firstDefaultAssignment xs 0) 
 
 
