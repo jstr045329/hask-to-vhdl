0215ea3a47a563a18b4cb6bf28b37cacc401e663
Jim Strieter <jimstrieter@gmail.com>
2020-10-03 15:22:20 -0400

    Improved documentation

--- src/Rendering/InfoNameTools.hs
+++ src/Rendering/InfoNameTools.hs
@@ -4,28 +4,43 @@ import Tools.LogicTools
 import Text.Printf
 
 
+------------------------------------------------------------------------------------------------------------------------
+--                                Split an Information Name Into Its Component Parts
+------------------------------------------------------------------------------------------------------------------------
 splitName :: String -> [String]
 splitName s = splitOn "_" s 
 
 
+------------------------------------------------------------------------------------------------------------------------
+--                                   Determine What An Information Is By Its Name
+--
+-- This function checks if the first token in a signal name (when split on underscores) matches target.
+------------------------------------------------------------------------------------------------------------------------
+matchInfoPrefix :: String -> String -> Bool 
+matchInfoPrefix someString target = ((head (splitName someString)) == target)
+
+
 nameIsInput :: String -> Bool
-nameIsInput s
-    | ((head (splitName s)) == "i") = True 
-    | otherwise = False 
+nameIsInput s = matchInfoPrefix s "i"
     
 
 nameIsOutput :: String -> Bool
-nameIsOutput s
-    | ((head (splitName s)) == "o") = True 
-    | otherwise = False 
+nameIsOutput s = matchInfoPrefix s "o"
 
 
 nameIsSignal :: String -> Bool
-nameIsSignal s
-    | ((head (splitName s)) == "s") = True 
-    | otherwise = False 
+nameIsSignal s = matchInfoPrefix s "s"
 
 
+nameIsVariable :: String -> Bool
+nameIsVariable s = matchInfoPrefix s "v"
+
+
+------------------------------------------------------------------------------------------------------------------------
+--                                         Define A Suffix For Signal Names
+--
+-- Suffix specifies delay relative to the input that drives it.
+------------------------------------------------------------------------------------------------------------------------
 signalSuffixFormatStr :: String 
 signalSuffixFormatStr = "_%06d"
 
@@ -34,10 +49,13 @@ signalSuffix :: Integer -> String
 signalSuffix n = printf signalSuffixFormatStr n 
 
 
--- If a port name starts with the prefix i_ or o_, this function replaces
--- the prefix with the signal prefix s_. If the name has no such prefix, 
--- this function simply prepends it with s_. In all cases, the signal suffix
--- is appended at the end. 
+------------------------------------------------------------------------------------------------------------------------
+--                                         Convert Input Name to Signal Name
+--
+-- If a port name starts with the prefix i_ or o_, this function replaces the prefix with the signal prefix s_. If
+-- the name has no such prefix, this function simply prepends it with s_. In all cases, the signal suffix is appended
+-- at the end.
+------------------------------------------------------------------------------------------------------------------------
 portName2SigName :: String -> Integer -> String 
 portName2SigName s n
     | ((take 2 s) == "i_") = "s_" ++ (tail (tail s)) ++ (signalSuffix n)
@@ -45,10 +63,16 @@ portName2SigName s n
     | otherwise = "s_" ++ s ++ (signalSuffix n)
 
 
+------------------------------------------------------------------------------------------------------------------------
+--                                                 Parse An Integer
+------------------------------------------------------------------------------------------------------------------------
 readInt :: String -> Integer 
 readInt s = read s 
 
 
+------------------------------------------------------------------------------------------------------------------------
+--                                          Test If Characters Are Numbers
+------------------------------------------------------------------------------------------------------------------------
 numberChars :: String 
 numberChars = ['0'..'9']
 
@@ -61,23 +85,11 @@ allCharsAreNumbers :: String -> Bool
 allCharsAreNumbers s = logicalAnd ([charIsNum c | c <- s]) 
 
 
+------------------------------------------------------------------------------------------------------------------------
+--                                            Extract Number from String
+------------------------------------------------------------------------------------------------------------------------
 getNumberFromString :: String -> Maybe Integer
 getNumberFromString s
     | (allCharsAreNumbers (last (splitName s))) = Just (readInt (last (splitName s)))
     | otherwise = Nothing
-
-
-
-
--- incrementSuffix :: String -> String 
- -- incrementSuffix s = 
-    -- TODO: Flesh this out
-
-
-
-
-
-
-
-
 
