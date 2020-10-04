260606896730e8359d2e4c0f77e1aef1b7cff509
Jim Strieter <jimstrieter@gmail.com>
2020-10-04 09:41:04 -0400

    Updated documentation

--- src/Rendering/ClkRst.hs
+++ src/Rendering/ClkRst.hs
@@ -1,16 +1,20 @@
 module Rendering.ClkRst where
 import Rendering.InfoTypes
 
-----------------------------------------------------------------------------------------------------
---                            Define a Type to Convey Clock and Reset
-----------------------------------------------------------------------------------------------------
-
--- Type signatures get cluttered up when you keep typing 
+------------------------------------------------------------------------------------------------------------------------
+--                                      Define a Type to Convey Clock and Reset
+--
+-- Type signatures get cluttered up when you keep typing:
+--
 --      ... -> Information -> Information -> ...
--- over and over. Here is a type to wrap those up:
+--
+-- over and over. Here is a type to wrap those up.
+------------------------------------------------------------------------------------------------------------------------
 data ClkRst = ClkRst Information Information deriving (Eq, Show)
 
 
+------------------------------------------------------------------------------------------------------------------------
+--                                Here Is A Function To Generate The Type In One Word
+------------------------------------------------------------------------------------------------------------------------
 easyClkRst :: ClkRst
 easyClkRst = ClkRst easyClk easyRst
-
