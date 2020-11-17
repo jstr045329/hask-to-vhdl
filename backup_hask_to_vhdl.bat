40fd687bfd689a518dcb38811ba3dabebda919fa
Jim Strieter <jimstrieter@gmail.com>
2020-11-17 16:56:49 -0500

    Fixed a typo

--- backup_hask_to_vhdl.bat
+++ backup_hask_to_vhdl.bat
@@ -6,10 +6,10 @@ REM copying the new backup.
 
 REM Before renaming hask-to-vhdl to hask-to-vhd01, we first have to delete the previous
 REM hask-to-vhd01:
-rmdir /q /s D:\hask-to-vhdl_backups\hask-to-vhd01
+rmdir /q /s D:\hask-to-vhdl_backups\hask-to-vhd_01
 
 REM Now we can rename the most recent backup:
-ren D:\hask-to-vhdl_backups\hask-to-vhdl hask-to-vhd01
+ren D:\hask-to-vhdl_backups\hask-to-vhdl hask-to-vhdl_01
 
 REM Now copy the latest directory:
 Xcopy /E /I C:\Users\jstrieter\hask-to-vhdl D:\hask-to-vhdl_backups\hask-to-vhdl
